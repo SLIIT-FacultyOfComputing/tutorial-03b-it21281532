6484f4e32aaed800b86ba748213f5e2de7253985
it21281532 <99232552+it21281532@users.noreply.github.com>
2022-03-03 15:15:22 +0000

    tutorial 03 submitted

--- exercise04.cpp
+++ exercise04.cpp
@@ -5,7 +5,8 @@ void print(int len, int wth);
 void input(int &len, int &wth);
 
 // Do not change the main() function
-int main() {
+int main() 
+{
    int length = 10, width = 5;
    input(length, width);
    print(length, width);
